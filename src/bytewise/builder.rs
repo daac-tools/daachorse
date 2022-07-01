56316d0eff2ca181359ef2142e5d7238505b5feb
Koichi Akabe <vbkaisetsu@gmail.com>
2022-07-01 16:56:02 +0900

    Update src/bytewise/builder.rs
    Co-authored-by: Shunsuke Kanda <shnsk.knd@gmail.com>

--- src/bytewise/builder.rs
+++ src/bytewise/builder.rs
@@ -97,8 +97,8 @@ impl DoubleArrayAhoCorasickBuilder {
     /// The smaller the number is, the faster the construction time will be;
     /// however, the memory efficiency can be degraded.
     ///
-    /// A fixed length of memory is allocated in proportion to this value in construction. If an all
-    /// ocation error occurs during building the automaton even though the pattern set is small, try
+    /// A fixed length of memory is allocated in proportion to this value in construction. If an
+    /// allocation error occurs during building the automaton even though the pattern set is small, try
     /// setting a smaller value.
     ///
     /// # Arguments
