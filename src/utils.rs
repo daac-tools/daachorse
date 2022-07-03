8976235eb950f12666316f9b46d514dbc283c3d9
Koichi Akabe <vbkaisetsu@gmail.com>
2022-07-03 11:11:28 +0900

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,7 @@ pub trait FromU32 {
 
 #[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
 impl FromU32 for usize {
+    #[inline(always)]
     fn from_u32(src: u32) -> Self {
         // Since the pointer width is guaranteed to be 32 or 64, the following process always
         // succeeds.
