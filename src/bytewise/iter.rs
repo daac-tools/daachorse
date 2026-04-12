d095752f93d2982c19c186264e372853f5e2a749
Koichi Akabe <vbkaisetsu@gmail.com>
2026-04-12 11:59:02 +0900

    Remove deprecated LestmostFindIterator (#122)

--- src/bytewise/iter.rs
+++ src/bytewise/iter.rs
@@ -201,13 +201,6 @@ where
     pub(crate) pos: usize,
 }
 
-/// Alias for [`LeftmostFindIterator`] for backward compatibility. This will be removed in 2.0.
-#[deprecated(
-    since = "1.0.1",
-    note = "Renamed to `LeftmostFindIterator`; this alias will be removed in 2.0."
-)]
-pub type LestmostFindIterator<'a, P, V> = LeftmostFindIterator<'a, P, V>;
-
 impl<P, V> Iterator for LeftmostFindIterator<'_, P, V>
 where
     P: AsRef<[u8]>,
