4467c40db32bb35ed2e62873386af38acf35b158
Koichi Akabe <vbkaisetsu@gmail.com>
2026-08-13 17:28:11 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -201,6 +201,32 @@
 //!     .build(patterns)
 //!     .unwrap();
 //! ```
+//!
+//! ## Serialization
+//!
+//! daachorse supports serializing automata into byte sequences.
+//!
+//! **Note: A major version upgrade may break serialization format compatibility.**
+//!
+//! ```
+//! use daachorse::DoubleArrayAhoCorasick;
+//!
+//! let patterns = vec!["ab", "a", "abcd"];
+//! let pma = DoubleArrayAhoCorasick::<u32>::new(patterns).unwrap();
+//!
+//! let serialized = pma.serialize();
+//!
+//! // Safe version: If the origin of the serialized automaton is unknown and its safety is not
+//! // guaranteed, use `deserialize()`.
+//! let (pma_deserialized, _) = DoubleArrayAhoCorasick::<u32>::deserialize(&serialized).unwrap();
+//!
+//! // Unsafe version: If you build the automaton at compile time, `deserialize_unchecked()` is also
+//! // an option.
+//! let (pma_deserialized, _) = unsafe {
+//!     DoubleArrayAhoCorasick::<u32>::deserialize_unchecked(&serialized)
+//! };
+//! ```
+
 
 #![deny(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
